36bf7d571d15df26b3b83dc4a14744786c40ec94
pmmachadov <pmmachadov@gmail.com>
2024-04-15 20:22:40 +0200

    Refactor server code to use async/await and separate controllers in javascriptVanilla.js

--- javascriptVanilla.js
+++ javascriptVanilla.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const fs = require('fs').promises;
 const PORT = 3000;
 
-// Simulated Database
+// --- MODEL ---
 const db = {
     users: [
         { id: 1, name: "Alice", role: "admin" },
@@ -12,49 +13,19 @@ const db = {
     ]
 };
 
-// Models
 const UserModel = {
-    getUsers: function () {
-        return db.users;
+    getUsers: async () => db.users,
+    addUser: async user => {
+        db.users.push(user);
+        return user;
     }
 };
 
 const StatusModel = {
-    getStatus: function () {
-        return db.status[0];
-    }
+    getStatus: async () => db.status[0]
 };
 
-// Controllers
-const UsersController = {
-    getUsers: (res) => {
-        try {
-            const users = UserModel.getUsers();
-            if (users && users.length > 0) {
-                UsersView.renderUsers(res, users);
-            } else {
-                throw new Error('No users found');
-            }
-        } catch (error) {
-            res.writeHead(500);
-            res.end(JSON.stringify({ error: error.message }));
-        }
-    }
-};
-
-const StatusController = {
-    getStatus: (res) => {
-        try {
-            const status = StatusModel.getStatus();
-            StatusView.renderStatus(res, status);
-        } catch (error) {
-            res.writeHead(500);
-            res.end(JSON.stringify({ error: error.message }));
-        }
-    }
-};
-
-// Views
+// --- VIEW ---
 const UsersView = {
     renderUsers: (res, users) => {
         res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -69,24 +40,57 @@ const StatusView = {
     }
 };
 
-// Server
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        switch (req.url) {
-            case '/users':
-                UsersController.getUsers(res);
-                break;
-            case '/status':
-                StatusController.getStatus(res);
-                break;
-            default:
-                res.writeHead(404);
-                res.end('Sorry can\'t find that!');
-        }
-    } else {
-        res.writeHead(405);
-        res.end(`${req.method} not allowed`);
+// --- CONTROLLER ---
+const UsersController = {
+    getUsers: async (req, res) => {
+        const users = await UserModel.getUsers();
+        UsersView.renderUsers(res, users);
+    },
+    addUser: async (req, res) => {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', async () => {
+            const user = JSON.parse(body);
+            const newUser = await UserModel.addUser(user);
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(newUser));
+        });
     }
+};
+
+const StatusController = {
+    getStatus: async (req, res) => {
+        const status = await StatusModel.getStatus();
+        StatusView.renderStatus(res, status);
+    }
+};
+
+// Server setup
+const server = http.createServer(async (req, res) => {
+    const logMessage = `Received request on ${req.url} with method ${req.method}\n`;
+    console.log(logMessage);
+    try {
+        await fs.appendFile('request-log.txt', logMessage);
+    } catch (err) {
+        console.error('Error writing to log file:', err);
+    }
+
+    if (req.url === '/users' && req.method === 'GET') {
+        await UsersController.getUsers(req, res);
+    } else if (req.url === '/users' && req.method === 'POST') {
+        await UsersController.addUser(req, res);
+    } else if (req.url === '/status' && req.method === 'GET') {
+        await StatusController.getStatus(req, res);
+    } else {
+        res.writeHead(404);
+        res.end('Sorry can\'t find that!');
+    }
+});
+
+server.on('error', error => {
+    console.error('Server error:', error);
 });
 
 server.listen(PORT, () => {
